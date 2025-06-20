815faafae47f8e2cccdecf9751c6fb2b5e0cc9d2
Lifailon <deadepicboss@gmail.com>
2025-06-21 01:41:11 +0300

    update check dir for export grafana dashboards

--- Scripts/Export-Grafana-Dashboards.ps1
+++ Scripts/Export-Grafana-Dashboards.ps1
@@ -1,15 +1,20 @@
+$url = "http://ip:port"
 $cred = "login:password"
-$url = "ip:port"
+
+$exportPath = "${home}/Documents/Backup/grafana-dashboards"
+if (-not (Test-Path $exportPath)) {
+    New-Item -ItemType Directory $exportPath | Out-Null
+}
 
 $base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($cred))
 $headers = @{
     Authorization = "Basic $base64Auth"
 }
 
-$dashboards = Invoke-RestMethod "http://$url/api/search?type=dash-db" -Headers $headers
+$dashboards = Invoke-RestMethod "$url/api/search?type=dash-db" -Headers $headers
 foreach ($dashboard in $dashboards) {
     $title = $($dashboard.title -replace '[\\/:*?"<>|]','_' -replace "\s","-") + ".json"
     $uid = $dashboard.uid
-    Write-Host "Export ${title}"
-    Invoke-RestMethod "http://192.168.3.105:9091/api/dashboards/uid/${uid}" -Headers $headers -OutFile $title
+    Invoke-RestMethod "$url/api/dashboards/uid/${uid}" -Headers $headers -OutFile "$exportPath/$title"
+    Write-Host "Export ${title}  =>  $exportPath/$title"
 }
