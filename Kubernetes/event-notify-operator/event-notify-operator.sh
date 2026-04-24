3e3bebeb779d66ba62f05e75e69bc2c757961f22
Lifailon <deadepicboss@gmail.com>
2026-04-24 15:03:06 +0300

    update kube and operator notes

--- Kubernetes/event-notify-operator/event-notify-operator.sh
+++ Kubernetes/event-notify-operator/event-notify-operator.sh
@@ -17,16 +17,18 @@ else
                 exit 0
             fi
             REASON=$(jq -r '.[0].object.reason' "$BINDING_CONTEXT_PATH")
-            RAW_MSG=$(jq -r '.[0].object.message' "$BINDING_CONTEXT_PATH")
+            CONTENT=$(jq -r '.[0].object.message' "$BINDING_CONTEXT_PATH")
             OBJECT_KIND=$(jq -r '.[0].object.involvedObject.kind' "$BINDING_CONTEXT_PATH")
             OBJECT_NAME=$(jq -r '.[0].object.involvedObject.name' "$BINDING_CONTEXT_PATH")
-            ICON="💡"; [[ "$EVENT_TYPE" == "Warning" ]] && ICON="⚠️"
+            ICON="🔔"; [[ "$EVENT_TYPE" == "Warning" ]] && ICON="⚠️"
             MESSAGE=$(cat <<EOF
-${ICON} Kubernetes Event
+☸️ Kubernetes
 
-*Type:* ${EVENT_TYPE}/${REASON}
-*Object:* ${OBJECT_KIND}/${OBJECT_NAME}
-*Message:* ${RAW_MSG}
+*Type*: ${ICON} ${EVENT_TYPE}
+*Action*: ${REASON}
+*Object*: ${OBJECT_KIND}/${OBJECT_NAME}
+
+${CONTENT}
 EOF
 )
             shoutrrr send --url "$SHOUTRRR_TELEGRAM" --message "$MESSAGE"
