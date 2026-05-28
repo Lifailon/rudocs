c894ecaa667bdc3d9577311509e02b0d3a32229b
Lifailon <deadepicboss@gmail.com>
2026-05-28 17:46:35 +0300

    add jenkins pipeline for kubectl commands run

---
+++ Jenkins/kubectl-runner/kubectl.groovy
@@ -0,0 +1,157 @@
+def log = {
+    def m = [:]
+    m.stage = { echo "\n\u001B[35m=== [STAGE: ${STAGE_NAME}] ===\u001B[0m\n" }
+    m.info = { text -> echo "\u001B[34m${text}\u001B[0m" }
+    m.warn = { text -> echo "\u001B[33m${text}\u001B[0m" }
+    m.success = { text -> echo "\u001B[32m${text}\u001B[0m" }
+    m.error = { text -> echo "\u001B[31m${text}\u001B[0m" }
+    m.uncolor = { text -> echo "${text}" }
+    return m
+}()
+
+def K8S_TOKEN = ""
+
+pipeline {
+    agent {
+        label 'linux && (amd64 || arm64)'
+    }
+    options {
+        ansiColor("xterm")
+        timestamps()
+        timeout(time: 10, unit: "MINUTES")
+    }
+    environment {
+        // KUBECONFIG = "${WORKSPACE}/kubeconfig.yaml"
+        KUBECTLPATH = tool(
+            name: 'kubectl-amd64-1.36.0',
+            type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
+        )
+        PATH = "${KUBECTLPATH}:${env.PATH}"
+    }
+    parameters {
+        separator(
+            name: "separatorVault",
+            sectionHeader: "Vault",
+            separatorStyle: "border-color: blue",
+            sectionHeaderStyle: "font-size: 1.5em; font-weight: bold;"
+        )
+        string(
+            name: "vaultUrl",
+            defaultValue: "http://192.168.3.101:8200"
+        )
+        string(
+            name: "vaultPath",
+            defaultValue: "jenkins/k3s",
+            description: "Path to secrets in Vault (where the token or config key with kubeconfig contents is stored)"
+        )
+        credentials(
+            name: "vaultAppRole",
+            credentialType: "com.datapipe.jenkins.vault.credentials.VaultAppRoleCredential",
+            defaultValue: "k3s-approle",
+            description: "AppRole for read access to secrets from Vault"
+        )
+        separator(
+            name: "separatorKubernetes",
+            sectionHeader: "Kubernetes",
+            separatorStyle: "border-color: blue",
+            sectionHeaderStyle: "font-size: 1.5em; font-weight: bold;"
+        )
+        string(
+            name: "url",
+            defaultValue: "https://192.168.3.101:6443"
+        )
+        string(
+            name: "namespace",
+            defaultValue: "kube-system"
+        )
+        text(
+            name: "commands",
+            description: "Commands to execute (each command on a new line)",
+            defaultValue: """kubectl get nodes
+kubectl get pods
+kubectl top pods"""
+        )
+    }
+    stages {
+        stage("Get Kubernetes token from Vault") {
+            steps {
+                script {
+                    log.stage()
+                    // Конфигурация для подключения к Vault
+                    def vaultConfiguration = [
+                        vaultUrl:           params.vaultUrl,
+                        vaultCredentialId:  params.vaultAppRole,
+                        engineVersion:      2
+                    ]
+                    // Массив для извлечения секретов
+                    def vaultSecrets  = [
+                        [
+                            path: params.vaultPath,
+                            engineVersion: 2,
+                            secretValues: [
+                                // [
+                                //     envVar: "kubeconfig",
+                                //     vaultKey: "config"
+                                // ],
+                                [
+                                    envVar: "kubetoken",    // название переменной
+                                    vaultKey: "token"       // ключ в Vault
+                                ]
+                            ]
+                        ]
+                    ]
+                    // Метод извлечения секретов из Vault
+                    withVault(
+                        [
+                            configuration:  vaultConfiguration,
+                            vaultSecrets:   vaultSecrets
+                        ]
+                    ) {
+                        // Записываем содержимое конфигурации в файл
+                        // writeFile(
+                        //     file: "${WORKSPACE}/kubeconfig.yaml",
+                        //     text: kubeconfig
+                        // )
+                        // Передаем содержимое токена в глобальную переменную
+                        K8S_TOKEN = kubetoken
+                    }
+                }
+            }
+        }
+        // stage("Check kubectl tool and token") {
+        //     steps {
+        //         script {
+        //             log.stage()
+        //             sh "kubectl version --output=json || true"
+        //             echo K8S_TOKEN
+        //         }
+        //     }
+        // }
+        stage("Run kubectl commands") {
+            steps {
+                script {
+                    log.stage()
+                    def commands = params.commands.split("\n")
+                    for (cmd in commands) {
+                        cmd = cmd.replaceAll("kubectl", "").trim()
+                        log.warn("▶ kubectl ${cmd}")
+                        def results = sh(
+                            script: """
+                                set +x
+                                kubectl --insecure-skip-tls-verify \
+                                    --kubeconfig=/dev/null \
+                                    --server=${params.url} \
+                                    --namespace=${params.namespace} \
+                                    --token=${K8S_TOKEN} \
+                                    ${cmd}
+                            """,
+                            returnStatus: false,
+                            returnStdout: true
+                        )
+                        log.success(results)
+                    }
+                }
+            }
+        }
+    }
+}
