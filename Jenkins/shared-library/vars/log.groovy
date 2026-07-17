b7264380eb5b267e02e945b67786541b4dc3ac70
Lifailon <116945542+Lifailon@users.noreply.github.com>
2026-07-17 13:41:03 +0300

    add new methods to log shared library

--- Jenkins/shared-library/vars/log.groovy
+++ Jenkins/shared-library/vars/log.groovy
@@ -1,3 +1,13 @@
+def env() {
+    env.getEnvironment().each { key, value ->
+        if (value.startsWith("http")) {
+            echo "\u001B[32m${key}\u001B[0m: ${value}"
+        } else {
+            echo "\u001B[32m${key}\u001B[0m: \u001B[34m${value}\u001B[0m"
+        }
+    }
+}
+
 def stage() {
     echo "\n\u001B[35m=== [STAGE: ${STAGE_NAME}] ===\u001B[0m\n"
 }
@@ -22,6 +32,11 @@ def uncolor(Object text) {
     echo "${text}"
 }
 
+def secret(Object text) {
+    def textStr = text.toString()
+    warn("${textStr[0]} ${textStr[1..-1]}")
+}
+
 def cmd(String script) {
     def result = sh(
         script: script,
@@ -31,45 +46,66 @@ def cmd(String script) {
     return result
 }
 
-def objectYaml(Object object) {
-    def result = writeYaml(
-        data: object,
-        returnText: true 
-    )
-    success(result)
-    return result
-}
-
-def objectJson(Object object) {
+def json(Object data) {
+    def objectJson = (data instanceof String) ? readJSON(text: data) : data
     def result = writeJSON(
-        json: object,
+        json: objectJson,
         returnText: true,
         pretty: 4
     )
     success(result)
-    return result
+    return objectJson
 }
 
-def rawJson(String text) {
-    def result = readJSON(text: text)
+def yaml(Object data) {
+    def objectYaml = (data instanceof String) ? readYaml(text: data) : data
+    def result = writeYaml(
+        data: objectYaml,
+        returnText: true 
+    )
     success(result)
-    return result
+    return objectYaml
 }
 
-def rawYaml(String text) {
-    def result = readYaml(text: text)
-    success(result)
-    return result
+def file(String path) {
+    if (fileExists(path)) {
+        def result = readFile(file: path)
+        success(result)
+        return result
+    } else {
+        error("File not found")
+    }
 }
 
 def fileJson(String path) {
-    def result = readJSON(file: path)
-    success(result)
-    return result
+    if (fileExists(path)) {
+        def result = readJSON(file: path)
+        json(result)
+        return result
+    } else {
+        error("File not found")
+    }
 }
 
 def fileYaml(String path) {
-    def result = readYaml(file: path)
-    success(result)
-    return result
+    if (fileExists(path)) {
+        def result = readYaml(file: path)
+        yaml(result)
+        return result
+    } else {
+        error("File not found")
+    }
+}
+
+def pathJson(def data, String path) {
+    def objectJson = (data instanceof String) ? readJSON(text: data) : data
+    def value = path.split('\\.').inject(objectJson) { object, key -> object?."${key}" }
+    if (value != null) {
+        if (value instanceof Map || value instanceof List) {
+            json(value)
+        } else {
+            success(value.toString())
+        }
+    }
+    return value
 }
